b797960d3bbdff9520dd6081621b759d50ff52ce
acenglishbcm <acenglishbcm@b0748103-3aed-4be6-b880-02589c5ab642>
2015-10-20 18:54:18 +0000

    Few mods before we begin python3 migration

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-source /hgsc_software/PBSuite/pbsuiteVirtualEnv/bin/activate
+#source /hgsc_software/PBSuite/pbsuiteVirtualEnv/bin/activate
 
 #This is the path where you've install the suite.
 export SWEETPATH=/stornext/snfs5/next-gen/scratch/english/Jelly/DevJelly/trunk
